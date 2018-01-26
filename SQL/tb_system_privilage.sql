b2410f171757b5bdb8adb805ba005a0454f5815b
Leo <laixinming@aisino.com>
2018-01-26 12:01:41 +0800

    更新了sql文件。

--- SQL/tb_system_privilage.sql
+++ SQL/tb_system_privilage.sql
@@ -1,21 +1,19 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50637
- Source Host           : localhost:3306
- Source Schema         : aqsiqdv
+Source Server         : localhost
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : aqsiqdv
 
- Target Server Type    : MySQL
- Target Server Version : 50637
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
 
- Date: 24/12/2017 14:32:38
+Date: 2018-01-26 12:00:51
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for tb_system_privilage
@@ -27,25 +25,23 @@ CREATE TABLE `tb_system_privilage` (
   `action_id` int(10) unsigned NOT NULL COMMENT '操作ID',
   PRIMARY KEY (`id`),
   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='系统权限表';
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COMMENT='系统权限表';
 
 -- ----------------------------
 -- Records of tb_system_privilage
 -- ----------------------------
-BEGIN;
-INSERT INTO `tb_system_privilage` VALUES (1, 1, 1);
-INSERT INTO `tb_system_privilage` VALUES (2, 1, 2);
-INSERT INTO `tb_system_privilage` VALUES (3, 1, 3);
-INSERT INTO `tb_system_privilage` VALUES (4, 1, 20);
-INSERT INTO `tb_system_privilage` VALUES (5, 1, 14);
-INSERT INTO `tb_system_privilage` VALUES (6, 1, 15);
-INSERT INTO `tb_system_privilage` VALUES (7, 1, 17);
-INSERT INTO `tb_system_privilage` VALUES (8, 1, 19);
-INSERT INTO `tb_system_privilage` VALUES (9, 1, 16);
-INSERT INTO `tb_system_privilage` VALUES (10, 1, 18);
-INSERT INTO `tb_system_privilage` VALUES (11, 1, 21);
-INSERT INTO `tb_system_privilage` VALUES (12, 1, 22);
-INSERT INTO `tb_system_privilage` VALUES (13, 1, 23);
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `tb_system_privilage` VALUES ('1', '1', '1');
+INSERT INTO `tb_system_privilage` VALUES ('2', '1', '2');
+INSERT INTO `tb_system_privilage` VALUES ('3', '1', '3');
+INSERT INTO `tb_system_privilage` VALUES ('4', '1', '20');
+INSERT INTO `tb_system_privilage` VALUES ('5', '1', '14');
+INSERT INTO `tb_system_privilage` VALUES ('6', '1', '15');
+INSERT INTO `tb_system_privilage` VALUES ('7', '1', '17');
+INSERT INTO `tb_system_privilage` VALUES ('8', '1', '19');
+INSERT INTO `tb_system_privilage` VALUES ('9', '1', '16');
+INSERT INTO `tb_system_privilage` VALUES ('10', '1', '18');
+INSERT INTO `tb_system_privilage` VALUES ('11', '1', '21');
+INSERT INTO `tb_system_privilage` VALUES ('12', '1', '22');
+INSERT INTO `tb_system_privilage` VALUES ('13', '1', '23');
+INSERT INTO `tb_system_privilage` VALUES ('14', '1', '24');
+INSERT INTO `tb_system_privilage` VALUES ('15', '1', '25');
